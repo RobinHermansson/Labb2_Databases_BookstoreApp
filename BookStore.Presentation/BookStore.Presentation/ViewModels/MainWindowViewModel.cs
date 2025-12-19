8400b831de9140daa6444f5890c54cfeb147c9c5
Robin Hermansson <robin.hermansson@stendahls.se>
2025-12-19 14:59:33 +0100

    Setup a store selector and an initial get of all stores

--- BookStore.Presentation/BookStore.Presentation/ViewModels/MainWindowViewModel.cs
+++ BookStore.Presentation/BookStore.Presentation/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Navigation;
 
 namespace BookStore.Presentation.ViewModels;
 
@@ -23,12 +24,40 @@ internal class MainWindowViewModel : ViewModelBase
 		}
 	}
 
+	private Store? _selectedStore;
+    public Store? SelectedStore 
+	{
+		get => _selectedStore;
+
+		set
+		{
+			_selectedStore = value;
+			RaisePropertyChanged();
+		}
+	}
+
+    private ObservableCollection<Store> _stores;
+
+	public ObservableCollection<Store> Stores
+	{
+		get { return _stores; }
+		set 
+		{ 
+			_stores = value;
+			RaisePropertyChanged();
+		}
+	}
+
+
 	public MainWindowViewModel()
     {
 
 		using var db = new BookstoreDBContext();
 		
 		Books = new ObservableCollection<Book>(db.Books.ToList());
+		Stores = new ObservableCollection<Store>(db.Stores.ToList());
+
+		SelectedStore = Stores.FirstOrDefault();
         
     }
 }
