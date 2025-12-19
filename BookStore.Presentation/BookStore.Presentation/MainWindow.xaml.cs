a9e1f3312cefde61638a6ac5566ac9060f8970c4
Robin Hermansson <robin.hermansson@hotmail.se>
2025-12-19 12:18:14 +0100

    Initial commit with basic setup

---
+++ BookStore.Presentation/BookStore.Presentation/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using BookStore.Presentation.ViewModels;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookStore.Presentation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MainWindowViewModel _mainWindowViewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = _mainWindowViewModel = new MainWindowViewModel();
+        }
+    }
+}
---
+++ BookStore.Presentation/BookStore.Presentation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CompanyDemo.Presentation.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Presentation.ViewModels;
+
+internal class MainWindowViewModel : ViewModelBase
+{
+
+	private ObservableCollection<Book> _test;
+
+	public ObservableCollection<Book> Test
+	{
+		get { return _test; }
+		set { 
+			_test = value;
+			RaisePropertyChanged();
+		}
+	}
+
+	public MainWindowViewModel()
+    {
+
+		Test = new ObservableCollection<Book>() 
+		{ 
+			new Book() { Id=1, Name="Test book" },
+			new Book() { Id=2, Name="Second test book"}
+		};
+        
+    }
+}
+
+public class Book
+{
+    public int Id { get; set; }
+	public string Name { get; set; }
+}
